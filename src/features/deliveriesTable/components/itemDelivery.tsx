12312d6663df43cd2853a58a9a4c36c991e5bc47
vitorsaa2k <vitoradriel66@gmail.com>
2025-04-14 20:55:51 -0300

    Move remove button to right side

--- src/features/deliveriesTable/components/itemDelivery.tsx
+++ src/features/deliveriesTable/components/itemDelivery.tsx
@@ -9,14 +9,24 @@ export function ItemDelivery({ delivery }: { delivery: Delivery }) {
 		removeDelivery(delivery);
 	}, [delivery, removeDelivery]);
 	return (
-		<div className="border border-black p-2 rounded" key={delivery.id}>
-			<p>Valor: {delivery.finalValue}</p>
-			<p>Bairro: {delivery.neighborhood}</p>
-			<p>Plataforma: {delivery.source}</p>
-			<p>Motoboy: {delivery.motoboy.name}</p>
-			<Button onClick={handleRemoveDelivery} className="hover:bg-red-400">
-				Remover
-			</Button>
+		<div
+			className="border border-black p-2 rounded flex flex-col"
+			key={delivery.id}
+		>
+			<div>
+				<p>Valor: {delivery.finalValue}</p>
+				<p>Bairro: {delivery.neighborhood}</p>
+				<p>Plataforma: {delivery.source}</p>
+				<p>Motoboy: {delivery.motoboy.name}</p>
+			</div>
+			<div className="flex justify-end mt-1">
+				<Button
+					onClick={handleRemoveDelivery}
+					className="hover:bg-red-400 justify-end"
+				>
+					Remover
+				</Button>
+			</div>
 		</div>
 	);
 }
