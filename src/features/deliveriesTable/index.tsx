e15a23c3f2d356140422570f2c849f1f7fecbb11
vitorsaa2k <vitoradriel66@gmail.com>
2025-04-14 20:47:48 -0300

    add margin to deliveryTable

--- src/features/deliveriesTable/index.tsx
+++ src/features/deliveriesTable/index.tsx
@@ -5,7 +5,7 @@ import { ItemDelivery } from "./components/itemDelivery";
 export function DeliveryTable() {
 	const deliveryList = useDeliveriesStore(state => state.deliveryList);
 	return (
-		<div className="grid gap-2 grid-cols-4">
+		<div className="grid gap-2 grid-cols-4 m-2">
 			{deliveryList.map(delivery => (
 				<ItemDelivery key={delivery.id} delivery={delivery} />
 			))}
