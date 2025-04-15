8eaa0bf6079975381393d10f324dc55b32bfb179
vitorsaa2k <vitoradriel66@gmail.com>
2025-04-14 22:42:11 -0300

    make motoboy input column

--- src/features/deliveryForm/components/inputs/motoboy.tsx
+++ src/features/deliveryForm/components/inputs/motoboy.tsx
@@ -2,9 +2,9 @@ import { ComboboxMotoboy } from "../comboboxMotoboy";
 
 export function SelectMotoboyInput() {
 	return (
-		<>
+		<div className="flex flex-col">
 			<label>Motoboy</label>
 			<ComboboxMotoboy />
-		</>
+		</div>
 	);
 }
