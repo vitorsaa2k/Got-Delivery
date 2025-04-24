b8da27ac7265035dc95e95665e9dfc29fa937f62
vitorsaa2k <vitoradriel66@gmail.com>
2025-04-23 22:35:05 -0300

    add api endpoint for handling date filtered deliveries

--- src/app/api/delivery/date/[date]/route.ts
+++ src/app/api/delivery/date/[date]/route.ts
@@ -1,26 +1,33 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+function getDateRange(day: string) {
+	const parsedDate = day.split("T").shift();
+	if (parsedDate === undefined)
+		return { start: new Date(), end: new Date(), error: true };
+	const start = new Date(parsedDate);
+	const end = new Date(parsedDate);
+	end.setDate(end.getDate() + 1);
+	return { start, end };
+}
+
 export async function GET(
 	req: NextRequest,
-	params: Promise<{ params: { date: Date } }>
+	{ params }: { params: { date: string } }
 ) {
-	console.log(await params);
-	const { date } = (await params).params;
-	/* let error;
-	const deliveries = await prisma.delivery
-		.findMany({
-			where: {
-				date: {
-					gte: date,
-					lt: date,
-				},
+	const { date } = await params;
+	const { start, end, error } = getDateRange(await date);
+	if (error) return NextResponse.json({ error });
+	const deliveries = await prisma.delivery.findMany({
+		where: {
+			date: {
+				gte: start,
+				lt: end,
 			},
-			include: {
-				motoboy: true,
-			},
-		})
-		.catch(err => (error = err));
-	console.log(deliveries); */
-	return NextResponse.json({ res: date });
+		},
+		include: {
+			motoboy: true,
+		},
+	});
+	return NextResponse.json({ deliveries, error });
 }
