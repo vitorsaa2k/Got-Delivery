a42ac52cb52ce0f6fb8c684d3bfa35d65fa7d511
vitorsaa2k <vitoradriel66@gmail.com>
2025-05-02 16:03:32 -0300

    add edit button to deliveryItem

--- src/features/deliveriesTable/components/itemDelivery.tsx
+++ src/features/deliveriesTable/components/itemDelivery.tsx
@@ -2,13 +2,19 @@
 import { Button } from "@/components/ui/button";
 import { useDeliveriesStore } from "@/stores/deliveriesStore";
 import { Delivery } from "@/types/global/types";
+import { Pencil } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 
 export function ItemDelivery({ delivery }: { delivery: Delivery }) {
+	const router = useRouter();
 	const removeDelivery = useDeliveriesStore(state => state.removeDelivery);
 	const handleRemoveDelivery = useCallback(() => {
 		removeDelivery(delivery);
 	}, [delivery, removeDelivery]);
+	const editDelivery = useCallback(() => {
+		router.push(`/delivery/${delivery.id}`);
+	}, [router, delivery.id]);
 	return (
 		<div
 			className="border border-black p-2 rounded flex flex-col"
@@ -20,7 +26,13 @@ export function ItemDelivery({ delivery }: { delivery: Delivery }) {
 				<p>Plataforma: {delivery.source}</p>
 				<p>Motoboy: {delivery.motoboy.name}</p>
 			</div>
-			<div className="flex justify-end mt-1">
+			<div className="flex justify-end items-center mt-1 gap-2">
+				<button
+					className="hover:bg-gray-300 hover:cursor-pointer p-2 rounded"
+					onClick={editDelivery}
+				>
+					<Pencil size={24} />
+				</button>
 				<Button
 					onClick={handleRemoveDelivery}
 					className="hover:bg-red-400 justify-end"
