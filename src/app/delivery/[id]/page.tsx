2b1aeab8049308bce81dd79290d5484dd9b6a4df
vitorsaa2k <vitoradriel66@gmail.com>
2025-05-14 21:50:38 -0300

    add delete button to editDeliveryPage

--- src/app/delivery/[id]/page.tsx
+++ src/app/delivery/[id]/page.tsx
@@ -1,13 +1,21 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { DeliveryForm } from "@/features/deliveryForm";
 import { fetchDeliveryById } from "@/services/delivery";
+import { useDeliveriesStore } from "@/stores/deliveriesStore";
 import { Delivery } from "@/types/global/types";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 
 export default function EditDeliveryPage() {
 	const [delivery, setDelivery] = useState<Delivery>();
+	const removeDelivery = useDeliveriesStore(state => state.removeDelivery);
 	const params = useParams();
+	const router = useRouter();
+	const handleDeleteDelivery = useCallback(() => {
+		if (delivery) removeDelivery(delivery);
+		router.back();
+	}, [delivery, removeDelivery, router]);
 	useEffect(() => {
 		fetchDeliveryById(`${params.id}`).then(data => setDelivery(data));
 	}, [params.id]);
@@ -15,6 +23,9 @@ export default function EditDeliveryPage() {
 	return (
 		<>
 			<DeliveryForm initialDelivery={delivery} />
+			<Button className="hover:cursor-pointer" onClick={handleDeleteDelivery}>
+				Delete
+			</Button>
 		</>
 	);
 }
