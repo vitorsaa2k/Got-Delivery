e6e6d32eacb17af27ea8853ec8b83a3bbb264a08
vitorsaa2k <vitoradriel66@gmail.com>
2025-05-25 11:46:14 -0300

    change hover bg change on loginButton

--- src/components/homePage/header/login/loginButton.tsx
+++ src/components/homePage/header/login/loginButton.tsx
@@ -1,6 +1,6 @@
 export function LoginButton() {
 	return (
-		<button className="bg-primary-foreground text-[20px] text-primary hover:bg-slate-400 rounded-r-none rounded-l-2xl border-primary border py-3 px-3">
+		<button className="bg-primary-foreground text-[20px] text-primary hover:bg-gray-200 rounded-r-none rounded-l-2xl border-primary border py-3 px-3">
 			Fazer Login
 		</button>
 	);
