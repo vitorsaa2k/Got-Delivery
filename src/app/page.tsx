3321fe248be148ba13a55c495e86139f5898e9d5
vitorsaa2k <vitoradriel66@gmail.com>
2025-05-25 11:49:35 -0300

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 import { HomePage } from "@/components/homePage";
-import { removeTimeFromDate } from "@/utils/removeTimeDate";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
 	/* const router = useRouter();
