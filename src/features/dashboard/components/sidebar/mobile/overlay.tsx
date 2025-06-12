928085adc81df9f3ff93a488bce90f0ccc743f5f
vitorsaa2k <vitoradriel66@gmail.com>
2025-06-12 15:29:45 -0300

    add mobileSidebar

---
+++ src/features/dashboard/components/sidebar/mobile/overlay.tsx
@@ -0,0 +1,18 @@
+import { RefObject } from "react";
+
+export function Overlay({
+	ref,
+	showSidebar,
+}: {
+	ref: RefObject<HTMLDivElement | null>;
+	showSidebar: boolean;
+}) {
+	return (
+		<div
+			ref={ref}
+			className={`w-screen inset-0 bg-black/20 fixed transition-opacity duration-300 ${
+				showSidebar ? "opacity-100" : "opacity-0"
+			}`}
+		></div>
+	);
+}
