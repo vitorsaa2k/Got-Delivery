976bbaf17f02bcd0d519a1ebde28543e92630c4a
vitorsaa2k <vitoradriel66@gmail.com>
2025-06-14 13:59:01 -0300

    invalidate query on motoboy delete

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: ReactNode }) {
 	return (
