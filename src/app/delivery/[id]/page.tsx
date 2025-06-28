44dd831d6d868dd0e48030d20b9ab6a24fdee346
vitorsaa2k <vitoradriel66@gmail.com>
2025-06-28 14:25:50 -0300

    make edit delivery page use dashboardLayout

--- src/app/delivery/[id]/page.tsx
+++ src/app/delivery/[id]/page.tsx
@@ -1,48 +1,10 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { DeliveryForm } from "@/features/deliveryForm";
-import { deleteDelivery, fetchDeliveryById } from "@/services/delivery";
-import { useDeliveriesStore } from "@/stores/deliveriesStore";
-import { Delivery } from "@/types/global/types";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useParams, useRouter } from "next/navigation";
-import { useCallback } from "react";
-import { toast } from "sonner";
+import { EditDeliveryForm } from "@/features/dashboard/components/delivery/editDeliveryForm";
+import { DashboardLayout } from "@/features/dashboard/layout";
 
 export default function EditDeliveryPage() {
-	const params = useParams();
-	const router = useRouter();
-	const queryClient = useQueryClient();
-	const { data: queryDelivery } = useQuery({
-		queryKey: ["delivery"],
-		queryFn: () => fetchDeliveryById(`${params.id}`),
-	});
-	const removeDelivery = useDeliveriesStore(state => state.removeDelivery);
-	const deleteDeliveryMutator = useMutation({
-		mutationFn: async (delivery: Delivery) => {
-			return await deleteDelivery(delivery);
-		},
-		onSuccess: async (delivery: Delivery) => {
-			removeDelivery(delivery);
-			await queryClient.invalidateQueries({ queryKey: ["deliveryList"] });
-			toast("Delivery removido com sucesso");
-			router.back();
-		},
-	});
-	const handleDeleteDelivery = useCallback(async () => {
-		if (queryDelivery) deleteDeliveryMutator.mutate(queryDelivery);
-	}, [queryDelivery, deleteDeliveryMutator]);
-
 	return (
-		<div className="p-9">
-			<DeliveryForm initialDelivery={queryDelivery} />
-			<Button
-				disabled={deleteDeliveryMutator.isPending}
-				className="hover:cursor-pointer"
-				onClick={handleDeleteDelivery}
-			>
-				Deletar
-			</Button>
-		</div>
+		<DashboardLayout>
+			<EditDeliveryForm />
+		</DashboardLayout>
 	);
 }
