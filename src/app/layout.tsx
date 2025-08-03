7d88d9b934c26ea9ae723656953e497baa8337fd
vitorsaa2k <vitoradriel66@gmail.com>
2025-08-03 18:02:16 -0300

    test deploy fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,16 +51,6 @@ export default function RootLayout({
 				async
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
 			></Script>
-			<Script
-				id="google-tag"
-				dangerouslySetInnerHTML={{
-					__html: ` window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`,
-				}}
-			/>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
