91f612d24aab34fa54497e7dddd40f0de6476331
vitorsaa2k <vitoradriel66@gmail.com>
2025-08-04 08:35:39 -0300

    temporarialy remove homepagenav until its not ready

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,11 @@
 import { LoginButtons } from "./login";
 import { Logo } from "./logo";
-import { HomePageNav } from "./nav";
 
 export function HomePageHeader() {
 	return (
 		<header className="flex justify-center gap-16 py-4 sticky top-0">
 			<Logo />
-			<HomePageNav />
+			{/* <HomePageNav /> */}
 			<LoginButtons />
 		</header>
 	);
