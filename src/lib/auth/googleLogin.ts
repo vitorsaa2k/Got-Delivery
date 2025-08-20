1947207b9a04c80fae50814ad1bc850fd3381ed7
vitorsaa2k <vitoradriel66@gmail.com>
2025-08-19 22:36:50 -0300

    make create company with empty name on googleLogin

--- src/lib/auth/googleLogin.ts
+++ src/lib/auth/googleLogin.ts
@@ -4,22 +4,22 @@ import { hash } from "bcrypt";
 
 export async function handleGoogleLogin(user: User) {
 	const company = await prisma.company.findUnique({ where: { id: user.id } });
+	if (company) return true;
+
 	const password = await hash(crypto.randomUUID(), 10);
-	if (!company) {
-		await prisma.company.create({
-			data: {
-				name: `${user.name}`,
-				email: `${user.email}`,
-				password: password,
-				id: user.id,
-			},
-		});
-		await prisma.verificationCode.create({
-			data: {
-				companyId: user.id,
-				verificationCode: null,
-			},
-		});
-	}
+	await prisma.company.create({
+		data: {
+			name: "",
+			email: `${user.email}`,
+			password: password,
+			id: user.id,
+		},
+	});
+	await prisma.verificationCode.create({
+		data: {
+			companyId: user.id,
+			verificationCode: null,
+		},
+	});
 	return true;
 }
