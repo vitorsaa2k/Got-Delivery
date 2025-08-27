701a045d74b7d1df8ed7c68854f3bff1b0cd4534
vitorsaa2k <vitoradriel66@gmail.com>
2025-08-27 08:19:43 -0300

    create company update name endpoint

---
+++ src/app/api/company/[id]/route.ts
@@ -0,0 +1,17 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+	req: NextRequest,
+	{ params }: { params: Promise<{ id: string }> }
+) {
+	const { id } = await params;
+	const body: { name: string } = await req.json();
+	const company = await prisma.company.update({
+		where: { id },
+		data: {
+			name: body.name,
+		},
+	});
+	NextResponse.json({ ok: true, data: company }, { status: 200 });
+}
